c1a9865b225232109fb1b43c927b344aadc9c890
geoseo360 <geoseo360@gmail.com>
2024-04-25 19:28:12 +0400

    Commit

---
+++ data.js
@@ -0,0 +1,249 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.14275681516246053,
+        "pitch": 0.15320380320039817,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2886476101991988,
+          "pitch": 0.05395643761986868,
+          "rotation": 0,
+          "target": "3-bathroom"
+        },
+        {
+          "yaw": 3.031078037588955,
+          "pitch": 0.06800954741684606,
+          "rotation": 0,
+          "target": "4-balcony"
+        },
+        {
+          "yaw": 2.7131788960135976,
+          "pitch": 0.2825290331745407,
+          "rotation": 0,
+          "target": "1-studio"
+        },
+        {
+          "yaw": -2.9414213633395363,
+          "pitch": 0.09224159254241115,
+          "rotation": 1.5707963267948966,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-studio",
+      "name": "Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.2315213562377494,
+        "pitch": 0.016053409564193544,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4043784009299003,
+          "pitch": 0.05695231570783754,
+          "rotation": 4.71238898038469,
+          "target": "3-bathroom"
+        },
+        {
+          "yaw": -0.07163215436259662,
+          "pitch": 0.06388067898379113,
+          "rotation": 0,
+          "target": "0-kitchen"
+        },
+        {
+          "yaw": -1.861981555850985,
+          "pitch": 0.17204331094241176,
+          "rotation": 0,
+          "target": "2-bedroom"
+        },
+        {
+          "yaw": -3.06521167530709,
+          "pitch": 0.11945626747362326,
+          "rotation": 0,
+          "target": "4-balcony"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.3643045617451186,
+        "pitch": 0.11101995376986373,
+        "fov": 1.167459575262189
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0877909081121224,
+          "pitch": 0.12045061800079893,
+          "rotation": 0,
+          "target": "4-balcony"
+        },
+        {
+          "yaw": -2.6208813806620483,
+          "pitch": 0.08197239994248307,
+          "rotation": 0,
+          "target": "1-studio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.44133093722658856,
+        "pitch": 0.13639415936375343,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0576524277748476,
+          "pitch": 0.15566501737103167,
+          "rotation": 0,
+          "target": "0-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.0523019945502003,
+        "pitch": 0.019582831483242913,
+        "fov": 1.3945837926101285
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9322951587669905,
+          "pitch": 0.1669095918899579,
+          "rotation": 0,
+          "target": "1-studio"
+        },
+        {
+          "yaw": 2.027424312119461,
+          "pitch": 0.08299712659831826,
+          "rotation": 0,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Asatiani-apartment",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
