87dc6ef5f7d319f47bc73501a63c6ffbed11c8e6
geoseo360 <167761580+geoseo360@users.noreply.github.com>
2024-04-26 14:40:27 +0400

    Update index.js

--- index.js
+++ index.js
@@ -171,15 +171,15 @@
 
   // Associate view controls with elements.
   var controls = viewer.controls();
-  var deviceOrientationControlMethod = new DeviceOrientationControlMethod();
-  controls.registerMethod('deviceOrientation', deviceOrientationControlMethod);
   controls.registerMethod('upElement',    new Marzipano.ElementPressControlMethod(viewUpElement,     'y', -velocity, friction), true);
   controls.registerMethod('downElement',  new Marzipano.ElementPressControlMethod(viewDownElement,   'y',  velocity, friction), true);
   controls.registerMethod('leftElement',  new Marzipano.ElementPressControlMethod(viewLeftElement,   'x', -velocity, friction), true);
   controls.registerMethod('rightElement', new Marzipano.ElementPressControlMethod(viewRightElement,  'x',  velocity, friction), true);
   controls.registerMethod('inElement',    new Marzipano.ElementPressControlMethod(viewInElement,  'zoom', -velocity, friction), true);
   controls.registerMethod('outElement',   new Marzipano.ElementPressControlMethod(viewOutElement, 'zoom',  velocity, friction), true);
-
+  var deviceOrientationControlMethod = new DeviceOrientationControlMethod();
+  controls.registerMethod('deviceOrientation', deviceOrientationControlMethod);
+	
   function sanitize(s) {
     return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
   }
